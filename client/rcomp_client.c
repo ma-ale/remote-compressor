65693bae852f7e6e73aac512c51e6a1a120149d6
Victoria Ambler <v.ambler@studenti.unipi.it>
2023-12-16 19:14:19 +0100

    prima implementazione della send_command

--- client/rcomp_client.c
+++ client/rcomp_client.c
@@ -52,3 +52,85 @@ int receive_response(int sd) {
 
 	return 0;
 }
+
+// manda un comando testuale al server come "quit" e "compress"
+// sd: descriptor del socket
+// str: stringa che contiene il comando
+// esempio: send_command(sd, "exit");
+int send_command(int sd, const char *com, const char *arg) {
+	// --- INVIO LUNGHEZZA --- //
+	// conversione a formato network (da big endian a little endian)
+	size_t com_len	 = strlen(com);
+	int	   msg_len	 = htonl(com_len);
+	size_t snd_bytes = send(sd, &msg_len, sizeof(int), 0);
+	if (snd_bytes < 0) {
+		fprintf(stderr, "Impossibile inviare dati comando: %s\n", strerror(errno));
+		return (-1);
+	}
+	printf("Inviati %ld bytes di lunghezza comando\n", snd_bytes);
+	// --- INVIO COMANDO --- //
+	// manda il comando byte per byte
+	size_t bytes_sent = 0;
+	char   buff[1];
+	while (1) {
+		buff[0]	  = com[bytes_sent];
+		snd_bytes = send(sd, buff, 1, 0);
+		if (snd_bytes < 0) {
+			fprintf(stderr, "Impossibile inviare dati comando: %s\n", strerror(errno));
+			return (-1);
+		}
+		bytes_sent += snd_bytes;
+		if (bytes_sent == (size_t)com_len) {
+			break;
+		} else if (bytes_sent > (size_t)com_len) {
+			printf(
+				"Invio comando fallito: inviati %ld byte in piu' della dimensione del "
+				"comando\n",
+				(bytes_sent - com_len)
+			);
+			return (-1);
+		}
+	}
+	printf("Inviati %ld bytes di comando\n", bytes_sent);
+
+	if (arg == NULL) {
+		printf("Il comando non prevede argomento: inviati 0 byte di argomento");
+		return 0;
+	}
+	// --- INVIO LUNGHEZZA ARGOMENTO--- //
+	// conversione a formato network (da big endian a little endian)
+	size_t arg_len = strlen(arg);
+	msg_len		   = htonl(arg_len);
+	snd_bytes	   = send(sd, &msg_len, sizeof(int), 0);
+	if (snd_bytes < 0) {
+		fprintf(stderr, "Impossibile inviare dati argomento: %s\n", strerror(errno));
+		return (-1);
+	}
+	printf("Inviati %ld bytes di lunghezza argomento\n", snd_bytes);
+	// --- INVIO ARGOMENTO --- //
+
+	// manda l'argomento byte per byte
+	bytes_sent = 0;
+	while (1) {
+		buff[0]	  = arg[bytes_sent];
+		snd_bytes = send(sd, buff, 1, 0);
+		if (snd_bytes < 0) {
+			fprintf(stderr, "Impossibile inviare dati argomento: %s\n", strerror(errno));
+			return (-1);
+		}
+		bytes_sent += snd_bytes;
+		if (bytes_sent == (size_t)arg_len) {
+			break;
+		} else if (bytes_sent > (size_t)arg_len) {
+			printf(
+				"Invio argomento fallito: inviati %ld byte in piu' della dimensione "
+				"dell' argomento\n",
+				(bytes_sent - arg_len)
+			);
+			return (-1);
+		}
+	}
+	printf("Inviati %ld bytes di argomento\n", bytes_sent);
+
+	return 0;
+}
